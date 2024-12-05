e4b81aba5e773c0417c9dcabc2dc58eafbf6b6a2
Saisha Verma <saisha.verma@qsstechnosoft.com>
2024-12-05 15:59:15 +0530

    third commit

---
+++ EmployeeManagementSystem/Program - Copy.cs
@@ -0,0 +1,28 @@
+using EmployeeManagementSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Enable Swagger (Optional for testing)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
